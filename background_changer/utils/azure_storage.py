411cdcf6bef676d38c8fa5e82862ef5037e15197
Hamze <hamzezn@gmail.com>
2024-03-05 15:14:02 +0330

    :art:

--- background_changer/utils/azure_storage.py
+++ background_changer/utils/azure_storage.py
@@ -1,4 +1,4 @@
-from azure.storage.blob import BlobServiceClient
+from azure.storage.blob import BlobServiceClient, ContentSettings
 
 from background_changer.settings import settings
 
@@ -17,8 +17,14 @@ def upload_image_to_blob_storage(image_path, image_name, container_name, content
     )
     container_client = blob_service_client.get_container_client(container_name)
     blob_client = container_client.get_blob_client(image_name)
+    if content_type:
+        blob_client.set_http_headers(
+            content_settings=ContentSettings(content_type=content_type),
+        )
     with open(image_path, "rb") as data:
-        blob_client.upload_blob(data, content_type=content_type)
+        blob_client.upload_blob(
+            data,
+        )
 
     blob_client = container_client.get_blob_client(image_name)
     return blob_client.url
